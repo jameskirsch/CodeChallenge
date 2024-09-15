f1d6b3303b8ea7d07b0e3d355f29bc517d6e6fad
James Kirsch <jjamesdk@yahoo.com>
2024-09-14 20:15:27 -0400

    Refactor and clean up EmployeeDataSeeder for improved readability

--- CodeChallenge/Data/EmployeeDataSeeder.cs
+++ CodeChallenge/Data/EmployeeDataSeeder.cs
@@ -10,58 +10,52 @@ namespace CodeChallenge.Data
 {
     public class EmployeeDataSeeder
     {
-        private EmployeeContext _employeeContext;
-        private const String EMPLOYEE_SEED_DATA_FILE = "resources/EmployeeSeedData.json";
+        private readonly EmployeeContext _employeeContext;
+        private const string EmployeeSeedDataFile = "resources/EmployeeSeedData.json";
 
         public EmployeeDataSeeder(EmployeeContext employeeContext)
         {
-            _employeeContext = employeeContext;
+            _employeeContext = employeeContext ?? throw new ArgumentNullException(nameof(employeeContext));
         }
 
         public async Task Seed()
         {
             if(!_employeeContext.Employees.Any())
             {
-                List<Employee> employees = LoadEmployees();
+                var employees = LoadEmployees();
                 _employeeContext.Employees.AddRange(employees);
-
                 await _employeeContext.SaveChangesAsync();
             }
         }
 
-        private List<Employee> LoadEmployees()
+        private static List<Employee> LoadEmployees()
         {
-            using (FileStream fs = new FileStream(EMPLOYEE_SEED_DATA_FILE, FileMode.Open))
-            using (StreamReader sr = new StreamReader(fs))
-            using (JsonReader jr = new JsonTextReader(sr))
-            {
-                JsonSerializer serializer = new JsonSerializer();
+            using var fs = new FileStream(EmployeeSeedDataFile, FileMode.Open);
+            using var sr = new StreamReader(fs);
+            using JsonReader jr = new JsonTextReader(sr);
+            var serializer = new JsonSerializer();
 
-                List<Employee> employees = serializer.Deserialize<List<Employee>>(jr);
-                FixUpReferences(employees);
+            var employees = serializer.Deserialize<List<Employee>>(jr);
+            FixUpReferences(employees);
 
-                return employees;
-            }
+            return employees;
         }
 
-        private void FixUpReferences(List<Employee> employees)
+        private static void FixUpReferences(List<Employee> employees)
         {
             var employeeIdRefMap = from employee in employees
                                 select new { Id = employee.EmployeeId, EmployeeRef = employee };
 
             employees.ForEach(employee =>
             {
-                
-                if (employee.DirectReports != null)
+                if (employee.DirectReports == null) return;
+                var referencedEmployees = new List<Employee>(employee.DirectReports.Count);
+                employee.DirectReports.ForEach(report =>
                 {
-                    var referencedEmployees = new List<Employee>(employee.DirectReports.Count);
-                    employee.DirectReports.ForEach(report =>
-                    {
-                        var referencedEmployee = employeeIdRefMap.First(e => e.Id == report.EmployeeId).EmployeeRef;
-                        referencedEmployees.Add(referencedEmployee);
-                    });
-                    employee.DirectReports = referencedEmployees;
-                }
+                    var referencedEmployee = employeeIdRefMap.First(e => e.Id == report.EmployeeId).EmployeeRef;
+                    referencedEmployees.Add(referencedEmployee);
+                });
+                employee.DirectReports = referencedEmployees;
             });
         }
     }
